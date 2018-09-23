df9ff68a99310ab8984c521b4f1ef8e46fb5e2af
Paul Shao <paulshaoyuqiao1@berkeley.edu>
2018-09-23 09:59:19 -0700

    Game Building in Progress

---
+++ Angry Oski/Assets/BreakUponImpact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakUponImpact : MonoBehaviour {
+
+    public float minimumForce = 1000;
+
+    float collisionForce(Collision2D coll){
+        //Estimate the force of collision 
+        float speed = coll.relativeVelocity.sqrMagnitude;
+        if (coll.collider.GetComponent<Rigidbody2D>()){
+            return speed * coll.collider.GetComponent<Rigidbody2D>().mass;
+        }
+        return speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collisionForce(collision) >= minimumForce){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Angry Oski/Assets/CollisionSpawnOnce.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSpawnOnce : MonoBehaviour {
+
+    public GameObject effect;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // Spawn Effect, then remove Script
+        Instantiate(effect, transform.position, Quaternion.identity);
+        Destroy(this);
+    }
+}
